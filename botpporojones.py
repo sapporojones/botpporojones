89f97b68251d12ce1959785f8652c64bbf853140
Sapporo Jones <sapporojones@localhost.localdomain>
2020-07-21 02:04:44 +0000

    adding shlookup

--- botpporojones.py
+++ botpporojones.py
@@ -24,11 +24,24 @@ from datetime import datetime, tzinfo, timedelta
 import pytz
 from bravado.client import SwaggerClient
 import bravado.exception
-
+from esipy import EsiApp
+from esipy import EsiClient
+from esipy import App
 from discord.ext import commands
 from dotenv import load_dotenv
 
 
+esi_app = EsiApp()
+
+#app = App.create(url="https://esi.evetech.net/latest/swagger.json?datasource=tranquility")
+
+client = EsiClient(
+    retry_requests=True,  # set to retry on http 5xx error (default False)
+    headers={'User-Agent': 'rando eve user just lookin at stuff'},
+    raw_body_only=False,  # default False, set to True to never parse response and only return raw JSON string content.
+)
+
+app = esi_app.get_latest_swagger
 
 load_dotenv()
 
@@ -99,7 +112,7 @@ async def ip(ctx, address):
 	full_reply = "\n **HACKER MODE ENGAGED**\n" + response1 + response2 + response3 + response4 + response5 
 	await ctx.send(full_reply)
 
-@bot.command(name='r', help='returns a random post from the specified subreddit')
+@bot.command(name='r', help='returns posts from the specified subreddit')
 async def r(ctx, sub_reddit):
 	reddit = praw.Reddit(client_id=reddit_clientID,
                      client_secret=reddit_clientSecret,
@@ -108,7 +121,7 @@ async def r(ctx, sub_reddit):
                      username=reddit_username)
 	random_submission = reddit.subreddit(sub_reddit).random()
 	if random_submission.over_18 == True:
-		submission_url = "**Adult or spoilered content detected, not posting**"
+		submission_url = "Adult content detected, not posting"
 	else:
 		submission_url = reddit.submission(random_submission).url
 	await ctx.send(submission_url)
@@ -192,14 +205,205 @@ async def corp(ctx, corporationName):
 
         await ctx.send(response)
 
-@bot.command(name='kick', help='kick a given user from the server')
+@bot.command(name='kick', help='kick a given user')
 @commands.has_role('admin')
 async def kick(ctx, user: discord.Member):
 	await ctx.guild.kick(user)
 	await ctx.send('**User has been removed**')
 
+@bot.command(name='shlookup', help='get info on a pilot')
+async def shlookup(ctx, pilot_name):
+    #channel = message.channel
+    pilot_lookup = app.op['post_universe_ids'](names=pilot_name)
+    response = client.request(pilot_lookup, raw_body_only=True)
+    response_data = response.raw
+    char_json_response = json.loads(response_data)
+    char_id = char_json_response["characters"][0]["id"]
+    
+    zkb_base_url = "https://zkillboard.com/api/stats/characterID/" + str(char_id) + "/"
+    zkb_response = requests.get(zkb_base_url)
+    zkb_json_response = zkb_response.json()
 
+    evewho_base_url = "https://evewho.com/api/character/" + str(char_id) + "/"
+    evewho_response = requests.get(evewho_base_url)
+    evewho_json_response = evewho_response.json()
+    corporation_id = evewho_json_response["info"][0]["corporation_id"]
+    
+    corp_name_get_url = "https://esi.evetech.net/latest/corporations/" + str(corporation_id) + "/?datasource=tranquility"
+    corp_name_response = requests.get(corp_name_get_url)
+    corp_name_json_response = corp_name_response.json()
+    
+    corp_name = corp_name_json_response["name"]
+    corp_member_count = corp_name_json_response["member_count"]
+    corp_ticker = corp_name_json_response["ticker"]
+    response = ""
+    response +=  "\n" 
+    response +=  f"{pilot_name} is id {char_id}" + "\n"
+    response +=  "\n" 
+    response +=  f"Here are some relevant urls for {pilot_name}:" + "\n"
+    response +=  f"Zkill: https://zkillboard.com/character/{char_id}/"+ "\n" 
+    response +=  f"EveWho: https://evewho.com/character/{char_id}/" + "\n" 
+    response +=  f"TEST Auth https://auth.pleaseignore.com/eve/character/{char_id}/"  + "\n"
+    response +=  "\n" 
+    response +=  f"{pilot_name} is a member of {corp_name} which has the ticker {corp_ticker} and has {corp_member_count} members"  + "\n"
+    response +=  f"Here are some relevant urls for {corp_name}:"  + "\n"
+    response +=  f"Zkill: https://zkillboard.com/corporation/{corporation_id}/"  + "\n"
+    response +=  f"EveWho: https://evewho.com/corporation/{corporation_id}/"  + "\n"
+    response +=  f"Dotlan: http://evemaps.dotlan.net/corp/{corporation_id}/"  + "\n"
+    response +=  "\n"  + "Pilot was born " + str(evewho_json_response['history'][0]['start_date']) + "\n"
+    response +=  f"{pilot_name} has the following zkb stats:"  + "\n"
 
+    try:
+        solo_kills = str(zkb_json_response["soloKills"])
+    except:
+        solo_kills = str(0)
+
+    response += ("Solo Kills: " + solo_kills) + "\n"
+    
+    try:
+        solo_losses = str(zkb_json_response["soloLosses"])
+    except:
+        solo_losses = str(0)
+    
+    response += ("Solo Losses: " + solo_losses) + "\n"
+    
+    try:
+        total_kills = str(zkb_json_response["shipsDestroyed"])
+
+        #only execute this block if the character has kills
+        zkb_kills_url = "https://zkillboard.com/api/kills/characterID/" + str(char_id) + "/"
+        zkb_get_kills = requests.get(zkb_kills_url)
+        zkb_kills_json = zkb_get_kills.json()
+        last_kill_hash = zkb_kills_json[0]["zkb"]["hash"]
+        last_kill_id_0 = zkb_kills_json[0]["killmail_id"]
+        last_kill_url = "https://esi.evetech.net/latest/killmails/" + str(last_kill_id_0) + "/" + str(last_kill_hash) + "/?datasource=tranquility"
+        last_kill_get = requests.get(last_kill_url)
+        last_kill_date_json = last_kill_get.json()
+        last_kill_datetime = last_kill_date_json["killmail_time"]
+    except:
+        total_kills = str(0)
+    
+    response += "Total Kills: " + total_kills + "\n"
+    
+    try:
+        total_losses = str(zkb_json_response["shipsLost"])
+
+        #only execute this block if the character has losses
+        zkb_losses_url = "https://zkillboard.com/api/losses/characterID/" + str(char_id) + "/"
+        zkb_get_losses = requests.get(zkb_losses_url)
+        zkb_losses_json = zkb_get_losses.json()
+        last_loss_id_0 = zkb_losses_json[0]["killmail_id"]
+        last_loss_hash = zkb_losses_json[0]["zkb"]["hash"]
+        last_loss_get_url = "https://esi.evetech.net/latest/killmails/" + str(last_loss_id_0) + "/" + str(last_loss_hash) + "/?datasource=tranquility"
+        last_loss_get = requests.get(last_loss_get_url)
+        last_loss_date_json = last_loss_get.json()
+        last_loss_datetime = last_loss_date_json["killmail_time"]
+    except:
+        total_losses = str(0)
+
+    response += "Total Losses: " + total_losses + "\n"
+   
+    response += "\n"
+    
+
+    try:
+        response += f"Last reported on a kill on {last_kill_datetime}" + "\n"
+        has_kill = 1
+    except:
+        response += "Character has never been on a kill" + "\n"
+        has_kill = 0
+    try:
+        response += f"Last reported on a loss on {last_loss_datetime}" + "\n"
+        has_loss = 1
+    except:
+        response += ("Character has never had a loss") + "\n"
+        has_loss = 0
+
+    response += ("\n")
+
+    if has_kill == 1:
+
+        if total_kills == 1:
+            last_kill_id_0 = zkb_kills_json[0]["killmail_id"]
+            response += "Last kill:\n"
+            response += f"https://zkillboard.com/kill/{last_kill_id_0}/" + "\n"
+        else:
+            try:
+                last_kill_id_0 = zkb_kills_json[0]["killmail_id"]
+                last_kill_id_1 = zkb_kills_json[1]["killmail_id"]
+                last_kill_id_2 = zkb_kills_json[2]["killmail_id"]
+                last_kill_id_3 = zkb_kills_json[3]["killmail_id"]
+                last_kill_id_4 = zkb_kills_json[4]["killmail_id"]
+                response += ("Last 5 kills:\n")
+                response += (f"https://zkillboard.com/kill/{last_kill_id_0}/") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_kill_id_1}/") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_kill_id_2}/") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_kill_id_3}/") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_kill_id_4}/") + "\n"
+            except:
+                last_kill_id_0 = zkb_kills_json[0]["killmail_id"]
+                response += ("Last kill:\n")
+                response += (f"https://zkillboard.com/kill/{last_kill_id_0}/") + "\n"
+    else:
+        return
+
+    response += ("\n")
+    if has_loss == 1:
+        if total_losses == 1:
+            last_kill_id_0 = zkb_kills_json[0]["killmail_id"]
+            response += ("Last loss:\n")
+            response += (f"https://zkillboard.com/kill/{last_loss_id_0}/") + "\n"
+        else:
+            try:
+                last_loss_id_0 = zkb_losses_json[0]["killmail_id"]
+                last_loss_id_1 = zkb_losses_json[1]["killmail_id"]
+                last_loss_id_2 = zkb_losses_json[2]["killmail_id"]
+                last_loss_id_3 = zkb_losses_json[3]["killmail_id"]
+                last_loss_id_4 = zkb_losses_json[4]["killmail_id"]
+                response += ("Last 5 losses:\n") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_loss_id_0}/") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_loss_id_1}/") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_loss_id_2}/") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_loss_id_3}/") + "\n"
+                response += (f"https://zkillboard.com/kill/{last_loss_id_4}/") + "\n"
+            except:
+                last_kill_id_0 = zkb_kills_json[0]["killmail_id"]
+                response += ("Last loss:\n")
+                response += (f"https://zkillboard.com/kill/{last_loss_id_0}/") + "\n"
+    else:
+        return
+    corp_history = []
+    i = len(evewho_json_response['history']) - 1
+    while len(corp_history) < 10:
+        corpid = evewho_json_response['history'][i]['corporation_id']
+        try:
+            corp_history.append(corpid)
+            i -= 1
+        except:
+            return
+    response += ("\nCorporate History: (up to last ten corps displayed)\n")
+    for entity in corp_history:
+        corp_name_get_url = "https://esi.evetech.net/latest/corporations/" + str(entity) + "/?datasource=tranquility"
+        corp_name_response = requests.get(corp_name_get_url)
+        corp_name_json_response = corp_name_response.json()
+        corp_name = corp_name_json_response["name"]
+        corp_print = "Character was in: " + str(corp_name)
+        
+        try:
+            alice_id = corp_name_json_response["alliance_id"]
+            alice_name_get_url = "https://esi.evetech.net/latest/alliances/" + str(alice_id) + "/?datasource=tranquility"
+            alice_name_response = requests.get(alice_name_get_url)
+            alice_name_json_response = alice_name_response.json()
+            corp_print += " which is a member of " + str(alice_name_json_response["name"])
+            response += (corp_print) + "\n"
+        except:
+            response += (corp_print) + "\n"
+        
+        
+        
+        #response += (str(entity))
+
+    await ctx.send(response)
 
 
 
